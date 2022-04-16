079c71745412dfc7237466a8f5a7f92c89632491
Brian Barnes <brianb12321@users.noreply.github.com>
2022-04-15 20:48:59 -0500

    Made getting environment variables easier; added unit tests for EnvironmentSecretManager (#12)
    Co-authored-by: Shandy-Man <103293699+Shandy-Man@users.noreply.github.com>

---
+++ DiscordBot.Tests/SecretManagerTests.cs
@@ -0,0 +1,30 @@
+﻿using DiscordBot.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DiscordBot.Tests
+{
+    public class SecretManagerTests
+    {
+        [Fact]
+        public void GetEnvironmentScope_IsSame()
+        {
+            ISecretManager manager = new EnvironmentSecretManager();
+            var scope = manager.SetEnvironmentScope();
+            Assert.Equal(scope, manager.GetEnvironmentScope());
+        }
+
+        [Fact]
+        public void GetDiscordBotSecret_IsSame()
+        {
+            ISecretManager manager = new EnvironmentSecretManager();
+            Environment.SetEnvironmentVariable("DISCORD_BOT_SECRET", "test", EnvironmentVariableTarget.Process);
+            var secret = manager.GetDiscordBotSecret();
+            Assert.Equal("test", secret);
+        }
+    }
+}
