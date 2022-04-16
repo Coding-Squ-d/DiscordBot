1b6ba143925348d5e8f19730afc91bfbe4b3aa47
Zachary Shand <ztshand@gmail.com>
2022-04-15 20:17:54 -0500

    Creating the tutor command

---
+++ DiscordBot/Modules/TutorModule.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Interactions;
+
+namespace DiscordBot.Modules
+{
+    public class TutorModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("tutor", "Need tutoring help? Run this command")]
+        public async Task Tutor()
+        {
+            await RespondAsync("Your tutor link is: ");
+        }
+    }
+}
